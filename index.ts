acc7d4f69af6c03e54bb461b9358c925be35c8ac
Alihassanbhatticoder <https://github.com/AliHassanbhatticoder/New-FirstProject-Calculator.git>
2024-03-28 00:57:02 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomenumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guessed a number 1-10: ",
+  },
+]);
+
+if (answers.userGuessedNumber === randomenumber) {
+  console.log("Congratulation! Your Number is Right Best Of Luck Keep it Up.");
+} else {
+  console.log("Your Guessed Number is Not Correct Try Again");
+}
